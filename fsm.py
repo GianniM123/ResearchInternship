9c705296416608e59cb0e072d57fbd498782b3a4
Gianni <gianni.monteban@ru.nl>
2022-02-04 15:39:16 +0100

    changed according to deal correctly with non-determinism, i.e. when two transitions match from state machine A match with one transition in state machine B, both pairs are included

--- fsm.py
+++ fsm.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from re import T
+from typing import List
 
 
 @dataclass
@@ -20,13 +20,11 @@ class FSM:
     initial_state: State
     transitions: list[Transition]
 
-
-
 @dataclass
-class StatePair:
-    state: State
-    matched_trans: list[Transition]
-    unmatched_trans: list[Transition]
+class ComparingStates:
+    states: tuple[State,State]
+    matching_trans: list[tuple[Transition,Transition]]
+    non_matching_trans: tuple[list[Transition],list[Transition]]
 
 class Singleton(type):
     _instances = {}
@@ -38,48 +36,48 @@ class Singleton(type):
 class FSM_Diff(metaclass=Singleton):
 
     # Transitions match if output is equal for a input
-    def matching_transitions(self,fsm_1,fsm_2):
-        matching_trans = []
+    def matching_transitions(self,fsm_1,fsm_2,out):
+        outcome = []
         for s1 in fsm_1.states:
             for s2 in fsm_2.states:
-                pair = (StatePair(s1,[],[]),StatePair(s2,[],[]))
+                used_trans1 = []
+                used_trans2 = []
+                state_compare = ComparingStates((s1,s2),[],([],[]))
                 for t1 in fsm_1.transitions:
-                    if t1.from_state == s1:
-                        matched = False # check for transitions that don't have the same input
-                        for t2 in fsm_2.transitions:
-                            if t2.from_state == s2 and t1.input == t2.input:
-                                if t1.output == t2.output:
-                                    pair[0].matched_trans.append(t1)
-                                    pair[1].matched_trans.append(t2)
-                                else:
-                                    pair[0].unmatched_trans.append(t1)
-                                    pair[1].unmatched_trans.append(t2)
-                                matched = True
-                        if matched == False:
-                            pair[0].unmatched_trans.append(t1) 
-                
+                    if (t1.from_state == s1 and out) or (t1.to_state == s1 and not out):
+                        used_trans1.append(t1)
                 for t2 in fsm_2.transitions:
-                    if t2.from_state == s2:
-                        matched = False
-                        for t1 in fsm_1.transitions:
-                            if t1.from_state == s1 and t1.input == t2.input:
-                                matched = True
-                        if matched == False:
-                            pair[1].unmatched_trans.append(t1) 
+                    if (t2.from_state == s2 and out) or (t2.to_state == s2 and not out):
+                        used_trans2.append(t2)
 
-                matching_trans.append(pair)
-        return matching_trans
-                        
+                for t1 in used_trans1:
+                    matched = False
+                    for t2 in used_trans2:
+                        if t1.input == t2.input and t1.output == t2.output:
+                            state_compare.matching_trans.append((t1,t2))
+                            matched = True
+                    if not matched:
+                        state_compare.non_matching_trans[0].append(t1)
                 
-                
-        
+                for t2 in used_trans2:
+                    matched = False
+                    for t1 in used_trans1:
+                        if t1.input == t2.input and t1.output == t2.output:
+                            matched = True
+                    if not matched:
+                        state_compare.non_matching_trans[1].append(t2)
+            
+                outcome.append(state_compare)
+        return outcome
+                    
 
 
     def simularity_score(self,fsm_1, fsm_2, k):
-        out_match_trans = self.matching_transitions(fsm_1,fsm_2)
-        print(out_match_trans[0][0].matched_trans)
-        print(out_match_trans[0][1].matched_trans)
+        out_match_trans = self.matching_transitions(fsm_1,fsm_2, True)
+        print(out_match_trans[0].non_matching_trans)
         
+        in_match_trans = self.matching_transitions(fsm_1,fsm_2,False)
+        print(in_match_trans[0].matching_trans)
 
 
     def algorithm(self, fsm_1, fsm_2, k):
