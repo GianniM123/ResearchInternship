1a27d6b4b97ede92e96368d97def85bf6c362284
Gianni <gianni.monteban@ru.nl>
2022-03-07 13:10:55 +0100

    add parameter to make it more clean

--- openssl_files/run_ssl.sh
+++ openssl_files/run_ssl.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 openssl_dir=../openssl_models
+out_dir=../out_openssl
 
 
 
 declare -a sat_solvers=("msat" "cvc4" "z3" "yices")
 # "btor" "picosat" "bdd" do not suppport logic
-mkdir -p ../out_openssl
+mkdir -p $out_dir
 for file1 in "$openssl_dir"/*/*/*
 do
   for file2 in "$openssl_dir"/*/*/*
@@ -17,13 +18,8 @@ do
         f2=${file2#${openssl_dir}}
         f1=${f1///}
         f2=${f2///}
-        name="../out_openssl/${f1%lea*}-${f2%lea*}-${solver}.dot"
+        name="${out_dir}/${f1%lea*}-${f2%lea*}-${solver}.dot"
         python3 ../algorithm/main.py --ref=$file1 --upd=$file2 -o $name -t 0.5 -s $solver -l 
-        
-        echo "${name}"
-    done
-  done
-done
 
-python3 add_date.py "../out/"
+python3 add_date.py "${out_dir}/"
 
