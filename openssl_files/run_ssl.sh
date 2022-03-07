285551c4dd318736035def59738d355c4925a26a
Gianni <gianni.monteban@ru.nl>
2022-03-07 13:52:53 +0100

    done keyword was gone

--- openssl_files/run_ssl.sh
+++ openssl_files/run_ssl.sh
@@ -19,7 +19,11 @@ do
         f1=${f1///}
         f2=${f2///}
         name="${out_dir}/${f1%lea*}-${f2%lea*}-${solver}.dot"
-        python3 ../algorithm/main.py --ref=$file1 --upd=$file2 -o $name -t 0.5 -s $solver -l 
+        python3 ../algorithm/main.py --ref=$file1 --upd=$file2 -o $name -t 0.5 -s $solver -l
+
+        echo ${name} 
+    done
+  done
+done
 
 python3 add_date.py "${out_dir}/"
-
