1a1f02f8bfbc5108ee7adb67fe55b9f81f4389f1
Gianni <gianni.monteban@ru.nl>
2022-04-25 14:30:45 +0200

    Read pairs from file, ignore pairs in the SMT

--- algorithm/main.py
+++ algorithm/main.py
@@ -5,6 +5,7 @@ import sys
 import networkx as nx
 
 from fsm import FSMDiff, SMT_SOLVERS
+from read_pairs import read_pairs
 import fsm
 import debug
 
@@ -14,15 +15,14 @@ def main():
     k_value = 0.5
     threshold = 0.2
     ratio = 1
-    matching_pair_one = None
-    matching_pair_two = None
+    matching_file = None
     reference_model = None
     updated_model = None
     reference_filename = None
     updated_filename = None
     output_file = "out.dot"
     try:
-        arguments = getopt.getopt(sys.argv[1:],"idlphs:k:t:r:a:b:o:",["time","debug","log","performance","help","smt","k_value","threshold","ratio","matching-first","matching-second", "ref=", "upd=", "out="])
+        arguments = getopt.getopt(sys.argv[1:],"idelphs:k:t:r:m:o:",["time","debug","equation","log","performance","help","smt","k_value","threshold","ratio","matching-file","ref=", "upd=", "out="])
 
         for current_arg, current_val in arguments[0]:
             if current_arg in ("-s", "--smt"):
@@ -45,10 +45,10 @@ def main():
                 threshold = float(current_val)
             elif current_arg in ("-r", "--ratio"):
                 ratio = float(current_val)
-            elif current_arg in ("-a","--matching-first"):
-                matching_pair_one = current_val
-            elif current_arg in ("-b","--matching-second"):
-                matching_pair_two = current_val
+            elif current_arg in ("-e", "--equation"):
+                fsm.equation = True
+            elif current_arg in ("-m","--matching-file"):
+                matching_file = current_val
             elif current_arg in ("--ref"):
                 reference_model = nx.drawing.nx_agraph.read_dot(current_val)
                 reference_filename = current_val
@@ -58,7 +58,7 @@ def main():
                 updated_model = nx.drawing.nx_agraph.read_dot(current_val)
                 updated_filename = current_val
             elif current_arg in ("-h", "--help"):
-                print("Usage: main.py --ref=<reference dot model> --upd=<updated dot model> [-l (add logging in out file) -d (print smt) -i (print time smt takes) -p (performance matrix) -o <output file> -s <smt-solver> -k <k value> -t <threshold value> -r <ratio value> -a <matching s1> -b <matching s2>]")
+                print("Usage: main.py --ref=<reference dot model> --upd=<updated dot model> [-l (add logging in out file) -d (print smt) -e (print linear equation output) -i (print time smt takes) -p (performance matrix) -o <output file> -s <smt-solver> -k <k value> -t <threshold value> -r <ratio value> -a <matching s1> -b <matching s2>]")
                 print("<smt-solver> options:")
                 for solver in SMT_SOLVERS:
                     print('\t' + solver)
@@ -70,11 +70,9 @@ def main():
         print("Model not set")
         return
 
-    matching_pair = None
-    if (matching_pair_one is None) ^ (matching_pair_two is None):
-        print("Not all matching pairs are set!")
-    elif matching_pair_one is not None and matching_pair_two is not None :
-        matching_pair = (matching_pair_one,matching_pair_two)
+    matching_pairs = None
+    if matching_file is not None:
+        matching_pairs = read_pairs(matching_file)
 
     for edge in reference_model.edges.data():
         if not "label" in edge[2]:
@@ -83,7 +81,7 @@ def main():
         if not "label" in edge[2]:
             edge[2]["label"] = ""
 
-    graph = FSMDiff().algorithm(reference_model,updated_model,k_value,threshold,ratio,matching_pair)
+    graph = FSMDiff().algorithm(reference_model,updated_model,k_value,threshold,ratio,matching_pairs)
     if fsm.logging:
         for idx,val in {"Reference":reference_filename, "Updated":updated_filename, "Output":output_file}.items():
             graph.graph.setdefault(idx,{})
